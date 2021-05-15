2adb653fe1499821d232eb1a37544f4890869d76
Jack McGill <mr.mcgilljh@gmail.com>
2021-05-15 16:18:31 +1000

    Add game instructions

--- app/src/main/java/au/edu/jcu/cp3406/multiples/GameActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/multiples/GameActivity.java
@@ -3,12 +3,14 @@ package au.edu.jcu.cp3406.multiples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
     private Game game;
     private TextView numberView;
+    private TextView instructions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +19,20 @@ public class GameActivity extends AppCompatActivity {
 
         game = new Game(5); // temporarily hardcoded
         numberView = findViewById(R.id.numberTextView);
+        instructions = findViewById(R.id.instructions);
 
         updateView();
 
     }
 
     public void updateView() {
+        if (game.getRoundType() == 0) {
+            instructions.setText(R.string.multiples_instructions);
+        } else instructions.setText(R.string.factors_instructions);
 
         numberView.setText(String.valueOf(game.getNumber()));
 
     }
+
 
 }
