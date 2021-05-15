fe005fca258d95b773c268809809f4d15988e97a
Jack McGill <mr.mcgilljh@gmail.com>
2021-05-15 15:34:44 +1000

    Simplify game logic

--- app/src/main/java/au/edu/jcu/cp3406/multiples/Game.java
+++ app/src/main/java/au/edu/jcu/cp3406/multiples/Game.java
@@ -1,19 +1,31 @@
 package au.edu.jcu.cp3406.multiples;
 
+import android.util.Log;
+
 public class Game {
-    private int rounds;
-    private int difficulty;
-    private Round round;
+    private final int numberOfRounds;
+    private int indexOfCurrentRound = 0;
+    private Round[] rounds;
 
-    public Game(int rounds, int difficulty) {
-        this.rounds = rounds;
-        this.difficulty = difficulty;
+    public Game(int numberOfRounds) {
+        this.numberOfRounds = numberOfRounds;
+        rounds = generateRounds();
     }
 
-    public void play() {
-        for (int i = 1; i < rounds; ++i) {
-            // create new round
+    private Round[] generateRounds() {
+        rounds = new Round[numberOfRounds];
+
+        for (int i = 0; i < numberOfRounds; i++) {
+            rounds[i] = new Round(0, i + 1);
+            Log.i("game", "Created round " + i);
         }
-    }
-}
 
+        return rounds;
+    }
+
+    public int getNumber() {
+        return rounds[indexOfCurrentRound].number;
+    }
+
+
+}
--- app/src/main/java/au/edu/jcu/cp3406/multiples/GameActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/multiples/GameActivity.java
@@ -3,12 +3,27 @@ package au.edu.jcu.cp3406.multiples;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private Game game;
+    private TextView numberView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        game = new Game(5); // temporarily hardcoded
+        numberView = findViewById(R.id.numberTextView);
+
+        numberView.setText(String.valueOf(game.getNumber()));
+
     }
+
+    public void updateView() {
+        
+    }
+
 }
