f74a27d7888d3089a7faddf475ad88b6818a5474
Jack McGill <mr.mcgilljh@gmail.com>
2021-05-18 19:19:17 +1000

    Add functionality to main menu button

--- app/src/main/java/au/edu/jcu/cp3406/multiples/GameOver.java
+++ app/src/main/java/au/edu/jcu/cp3406/multiples/GameOver.java
@@ -1,5 +1,6 @@
 package au.edu.jcu.cp3406.multiples;
 
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
@@ -54,5 +55,8 @@ public class GameOver extends AppCompatActivity {
     }
 
     public void mainMenuButtonPressed(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 }
