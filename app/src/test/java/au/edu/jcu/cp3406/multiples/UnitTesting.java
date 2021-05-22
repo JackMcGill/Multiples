9a7abc2722acc386499cc442de89b8748c696f4a
Jack McGill <mr.mcgilljh@gmail.com>
2021-05-22 21:02:31 +1000

    Update UnitTesting.java

--- app/src/test/java/au/edu/jcu/cp3406/multiples/UnitTesting.java
+++ app/src/test/java/au/edu/jcu/cp3406/multiples/UnitTesting.java
@@ -14,24 +14,52 @@ public class UnitTesting {
 
     @Test
     public void multiplesRoundCreation_isCorrect() {
-        Round round1 = new Round(0, 25);
+        Round round1 = new Round(0, 25, true);
         assertEquals(25, round1.getNumber());
         assertEquals(0, round1.getRoundType());
     }
 
     @Test
     public void factorsRoundCreation_isCorrect() {
-        Round round1 = new Round(1, 256);
+        Round round1 = new Round(1, 256, true);
         assertEquals(256, round1.getNumber());
         assertEquals(1, round1.getRoundType());
     }
 
     @Test
     public void gameCreation_isCorrect() {
-        Game game = new Game(5);
+        Game game = new Game(5, true);
         int number = game.getNumber();
         int type = game.getRoundType();
-        assertNotNull(number);
-        assertNotNull(type);
+        assertNotNull(number); // I've chosen to ignore the yellow warnings here, otherwise the test doesn't run, and I don't see how else to check for correct game creation.
+        assertNotNull(type); // as above ^
+    }
+
+    @Test
+    public void timerCreation_isCorrect() {
+        Timer timer = new Timer(30);
+        assertEquals(30, timer.getTimeLeft());
+    }
+
+    @Test
+    public void timerToString_isCorrect() {
+        Timer timer = new Timer(30);
+        assertEquals("30", timer.toString());
+    }
+
+    @Test
+    public void timerTick_isCorrect() {
+        Timer timer = new Timer(30);
+        timer.tick();
+        assertEquals(29, timer.getTimeLeft());
+    }
+
+    @Test
+    public void timerReset_isCorrect() {
+        Timer timer = new Timer(30);
+        timer.tick();
+        assertEquals(29, timer.getTimeLeft());
+        timer.reset();
+        assertEquals(30, timer.getTimeLeft());
     }
 }
